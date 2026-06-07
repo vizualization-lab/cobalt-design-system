9121c10d3f856de182e94b5b292a363a04e408c9
Lonny Gomes <code@lonnygomes.com>
2026-06-07 19:45:41 -0400

    feat(cli): set ANSI shadow effect to a solid off-white color

--- packages/cli/src/startup-art.ts
+++ packages/cli/src/startup-art.ts
@@ -16,6 +16,7 @@ export function renderStartupArt({ color = false, version }: StartupArtOptions):
   const wordmark = colorizeLines(
     cobaltWordmark,
     ['38;5;159', '38;5;153', '38;5;117', '38;5;111', '38;5;75', '38;5;69'],
+    '38;5;250',
     color,
   );
   const brand = colorize('COBALT', '38;5;111', color);
@@ -39,13 +40,48 @@ function colorize(value: string, code: string, color: boolean): string {
   return color ? `\u001B[${code}m${value}\u001B[0m` : value;
 }
 
-function colorizeLines(value: string, codes: string[], color: boolean): string {
+const SHADOW_CHARS = /[╔╗╚╝═║╠╣╦╩╬]/;
+
+function colorizeLines(
+  value: string,
+  blockCodes: string[],
+  shadowCode: string,
+  color: boolean,
+): string {
   if (!color) {
     return value;
   }
 
   return value
     .split('\n')
-    .map((line, index) => colorize(line, codes[index % codes.length], color))
+    .map((line, index) => colorizeShadow(line, blockCodes[index % blockCodes.length], shadowCode))
     .join('\n');
+}
+
+function colorizeShadow(line: string, blockCode: string, shadowCode: string): string {
+  let result = '';
+  let buffer = '';
+  let bufferIsShadow = false;
+
+  const flush = () => {
+    if (buffer === '') return;
+    result += `\u001B[${bufferIsShadow ? shadowCode : blockCode}m${buffer}\u001B[0m`;
+    buffer = '';
+  };
+
+  for (const char of line) {
+    if (char === ' ') {
+      flush();
+      result += char;
+      continue;
+    }
+    const isShadow = SHADOW_CHARS.test(char);
+    if (buffer !== '' && isShadow !== bufferIsShadow) {
+      flush();
+    }
+    bufferIsShadow = isShadow;
+    buffer += char;
+  }
+  flush();
+  return result;
 }
